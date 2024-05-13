75b10d0aa3f77c9750c514422d73b9bb6bbafbf6
Bruna Vitoria Romano <brunaclaudia468@gmail.com>
2024-05-13 09:55:39 -0300

    criando o codigo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        int Numero;
+        String Agencia;
+        String NomeCliente;
+        BigDecimal Saldo;
+
+        System.out.println("Olá! Por favor, insira o seu nome:");
+        NomeCliente = sc.nextLine();
+
+        System.out.println("Agora insira o número da agência:");
+        Numero = sc.nextInt();
+
+        System.out.println("Agora insira a sua agência bancária:");
+        Agencia = sc.nextLine();
+
+        System.out.println("Certo, agora digite o saldo da conta:");
+        Saldo = sc.nextBigDecimal();
+
+        //System.out.println($"Olá {sc.NomeCliente}, obrigado por criar uma conta em nosso banco, sua agência é {Agencia}, conta {Numero} e seu saldo {Saldo} já está disponível para saque");
     }
 }
